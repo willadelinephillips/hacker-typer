f24f7f510ad683303da3a38fe5aac830e1a8dfb6
Will <willadelinephillips@gmail.com>
2021-07-21 17:32:09 -0700

    Edits

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hacking into the Database...
         </p>
+        <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
