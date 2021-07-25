613fc20bf774a10ce20e8aab0f9990e81a1b9653
Will <willadelinephillips@gmail.com>
2021-07-24 20:15:56 -0700

    Phrases updated and keyboard sensitive
    Phrases now more accurately reflect scenario and keyboard response is accurate.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,62 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 import { generate } from './utils/words';
+import useKeyPress from './hooks/useKeyPress';
 
 const initialWords = generate();
 //console.log(initialWords);
 
 function App() {
+
+  const [leftPadding, setLeftPadding] = useState(
+    new Array(20).fill(' ').join(''),
+  );
+  const [outgoingChars, setOutgoingChars] = useState('');
+  const [currentChar, setCurrentChar] = useState(initialWords.charAt(0));
+  const [incomingChars, setIncomingChars] = useState(initialWords.substr(1));
+
+  useKeyPress(key => {
+    //1
+    let updatedOutgoingChars = outgoingChars;
+    let updatedIncomingChars = incomingChars;
+    
+    //2
+    if (key === currentChar) {
+      //3
+      if (leftPadding.length > 0) {
+        setLeftPadding(leftPadding.substring(1));
+      }
+      //4
+      updatedOutgoingChars += currentChar;
+      setOutgoingChars(updatedOutgoingChars);
+      
+      //5      
+      setCurrentChar(incomingChars.charAt(0));
+      
+      //6
+      updatedIncomingChars = incomingChars.substring(1);
+      if (updatedIncomingChars.split(' ').length < 10) {
+        updatedIncomingChars +=' ' + generate();
+      }
+      setIncomingChars(updatedIncomingChars);
+    }
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Hacking into the Database...
         </p>
+        
+        <p className="Character">
+          <span className="Character-out">
+            {(leftPadding + outgoingChars).slice(-20)}
+          </span>
+          <span className="Character-current">{currentChar}</span>
+          <span>{incomingChars.substr(0, 20)}</span>
+        </p>
+        
         <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
--- src/utils/phrases.js
+++ src/utils/phrases.js
@@ -2,9 +2,14 @@ export const phrases = () => {
 
     let hackerphrases = [
 
-        "Hacking the mainframe",
-        "Encoding the database",
-        "Decoding the file formatting"
+        "cmd admin launch edit",
+        "scan internal drive bypass",
+        "password override brute force",
+        "keygen access !ignore permissions",
+        "file upload !bypass admin",
+        "channel dox file reload !important",
+        "object override !admin",
+        "export int let filesize = max"
     
     ]
 
